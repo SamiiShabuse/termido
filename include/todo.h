84e855b1b40da368a964a796b53fdf6395ea8d76
Samii Shabuse <sshabuse@gmail.com>
2025-06-18 01:27:34 -0400

    feat(todo): define Task struct and max task limit for in-memory tracking

--- include/todo.h
+++ include/todo.h
@@ -1,6 +1,13 @@
 #ifndef TODO_H
 #define TODO_H
 
+#define MAX_TASKS 100
+
+typedef struct {
+    char description[256];
+    int completed; // 0 = not completed, 1 = completed
+} Task;
+
 void addTask(const char *desc);
 void listTasks(void);
 
