c67cb5e457750d6a15227e523eb17aef6fa97585
Samii Shabuse <sshabuse@gmail.com>
2025-06-18 01:03:57 -0400

    feat(todo): implement basic task fucntions addTask and listTasks

---
+++ src/todo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "todo.h"
+
+void addTask(const char *desc) {
+    printf("Task added: %s\n", desc);
+}
+
+void listTasks(void) {
+    printf("Listing tasks (placeholder)...\n");
+}
